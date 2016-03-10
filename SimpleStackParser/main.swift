d65fc78c35f9fafb62f10a7333b6c41ff6cd9503
MR <rakhmanov.m@gmail.com>
2016-03-10 20:07:03 +0300

    Initial Commit

---
+++ SimpleStackParser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleStackParser
+//
+//  Created by MIKHAIL RAKHMANOV on 10.03.16.
+//  Copyright © 2016 No Logo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
